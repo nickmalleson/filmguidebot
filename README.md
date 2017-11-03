76e24a44d9a4a74606319e71f66b5b0cbb6c0c4d
Nick Malleson <n.s.malleson@leeds.ac.uk>
2017-11-03 20:14:24 +0000

    finished v1, calculator bot works

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 # filmguidebot
+
 A Slack bot that tells you what films are on UK TV over  the next 7 days, and what their IMDB rating is
 
 I followed the tutorial [here](https://blog.scalac.io/2015/07/16/slack.html) to make the bot.
+
+## Configuration
+
+You need to provide an API key from Slack after creating a new Bot (see the [tutorial](https://blog.scalac.io/2015/07/16/slack.html)). Once you have this, create the following file: `src/main/resources/application.conf` with contents:
+
+```
+api {
+  key = "XXXX"
+
+  base.url = "https://slack.com/api/"
+}
+websocket.key = "M8eMPBM=7G(igdN-pP.FN0nK"
+```
+replacing `XXXX` with your key.
+
+## Run:
+
+Simply run with:
+
+```
+sbt clean run
+```
--- src/main/scala/CalculatorBot.scala
+++ src/main/scala/CalculatorBot.scala
@@ -5,8 +5,19 @@ import io.scalac.slack.MessageEventBus
 import io.scalac.slack.bots.AbstractBot
 import io.scalac.slack.common.{BaseMessage, Command, OutboundMessage}
 
+/**
+  * Extends AbstractBot, which is a thin wrapper around Akka’s Actor class
+  * providing few useful utility methods for bot authors and constraints a
+  * Bot should meet.
+  * @param bus For communication
+  */
 class CalculatorBot(override val bus: MessageEventBus) extends AbstractBot {
 
+  /**
+    * Return a help message.
+    * @param channel
+    * @return
+    */
   override def help(channel: String): OutboundMessage =
     OutboundMessage(channel,
       s"$name will help you to solve difficult math problems \\n" +
@@ -19,10 +30,20 @@ class CalculatorBot(override val bus: MessageEventBus) extends AbstractBot {
     "/" -> ((x: Double, y: Double) => x/y)
   )
 
+  /**
+    * How to behave when a message is received (?).
+    *
+    * Match on <code>Command</code> instances to determin what action to take.
+    * <code>Command</code> classes have a leading 'command', a list of arguments,
+    * and the raw message (which includes,e.g., the channel ID).
+    * @return
+    */
   override def act: Receive = {
-    case Command("calc", operation :: args, message) if args.length >= 1 =>
-      val op = possibleOperations.get(operation)
 
+    // First case is a correctly specified command with >0 arguments
+    case Command("calc", operation :: args, message) if args.length >= 1 =>
+
+      val op = possibleOperations.get(operation)
       val response = op.map(f => {
         val result = args.map(_.toDouble).reduceLeft( f(_,_) )
         OutboundMessage(message.channel, s"Results is: $result")
@@ -30,6 +51,7 @@ class CalculatorBot(override val bus: MessageEventBus) extends AbstractBot {
 
       publish(response)
 
+    // This case indicates that no arguments have been specified:
     case Command("calc", _, message) =>
       publish(OutboundMessage(message.channel, s"No arguments specified!"))
   }
