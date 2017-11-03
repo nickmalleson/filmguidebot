7fa63956bfedc27378584fd4ed479f57be26e8d1
Nick Malleson <n.s.malleson@leeds.ac.uk>
2017-11-03 17:42:37 +0000

    continuing to work through blog

--- src/main/scala/BotRunner.scala
+++ src/main/scala/BotRunner.scala
@@ -1,18 +1,32 @@
-import akka.actor.{ActorSystem, Props}
-import io.scalac.slack.MessageEventBus
+import akka.actor.{ActorContext, ActorRef, ActorSystem, Props}
 import io.scalac.slack.api.{BotInfo, Start}
-import io.scalac.slack.common.Shutdownable
+import io.scalac.slack.bots.system.{CommandsRecognizerBot, HelpBot}
 import io.scalac.slack.common.actors.SlackBotActor
-import io.scalac.slack.websockets.WebSocket
+import io.scalac.slack.common.{Shutdownable, UsersStorage}
+import io.scalac.slack.example.CalculatorBot
+import io.scalac.slack.{BotModules, MessageEventBus, Config => SlackConfig}
+import io.scalac.slack.websockets.{WSActor, WebSocket}
+
 
 /* Source:
 https://github.com/pjazdzewski1990/scala-slack-bot-example/blob/master/src/main/scala/io/scalac/slack/example/BotRunner.scala
  */
 
 object BotRunner extends Shutdownable {
-  val system = ActorSystem("SlackBotSystem")
+  val system = ActorSystem("SlackBotSystem") // The Slack Bot is written on top of Akka
   val eventBus = new MessageEventBus
-  val slackBot = system.actorOf(Props(classOf[SlackBotActor], new ExampleBotsBundle(), eventBus, this, None), "slack-bot")
+
+  // The SlackBotActor is the main class used. It takes a few parameters:
+  val slackBot = system.actorOf(
+    Props(
+      classOf[SlackBotActor],
+      new ExampleBotsBundle(), // BotModules instance
+      eventBus, // MessageEventsBus (for communication)
+      this, // A reference to the master class
+      None // An (optional?) ActorRef pointing to UserStorage actor
+    ),
+    "slack-bot")
+
   var botInfo: Option[BotInfo] = None
 
   def main(args: Array[String]) {
@@ -38,5 +52,20 @@ object BotRunner extends Shutdownable {
     slackBot ! WebSocket.Release
     system.shutdown()
     system.awaitTermination()
+  }
+
+  /**
+    * The BotModules bundle allows you to add multiple bot instances in one bundle
+    * using the registerModules function.
+    *
+    * (For now start three example bots: CommandsRecognizerBot, HelpBot and CalculatorBot
+    * (a bot that we will define - see the <code>CalculatorBot</code> class).
+    */
+  class ExampleBotsBundle() extends BotModules {
+    override def registerModules(context: ActorContext, websocketClient: ActorRef) = {
+      context.actorOf(Props(classOf[CommandsRecognizerBot], eventBus), "commandProcessor")
+      context.actorOf(Props(classOf[HelpBot], eventBus), "helpBot")
+      context.actorOf(Props(classOf[CalculatorBot], eventBus), "CalculatorBot")
+    }
   }
 }
